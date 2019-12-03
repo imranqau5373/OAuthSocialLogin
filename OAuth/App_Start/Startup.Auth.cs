b2acf3c409f6112931643990d187e770d9ef67a6
imranqau5373 <imranqau5373@gmail.com>
2019-12-03 08:57:42 +0500

    fts

---
+++ OAuth/App_Start/Startup.Auth.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.Owin;
+using Microsoft.Owin.Security.Cookies;
+using Microsoft.Owin.Security.Google;
+using Microsoft.Owin.Security.OAuth;
+using Owin;
+using OAuth.Providers;
+using OAuth.Models;
+using Microsoft.Owin.Security.Facebook;
+using OAuth.Facebook;
+using Owin.Security.Providers.GitHub;
+
+namespace OAuth
+{
+    public partial class Startup
+    {
+        public static OAuthAuthorizationServerOptions OAuthOptions { get; private set; }
+
+        public static string PublicClientId { get; private set; }
+
+        // For more information on configuring authentication, please visit https://go.microsoft.com/fwlink/?LinkId=301864
+        public void ConfigureAuth(IAppBuilder app)
+        {
+            // Configure the db context and user manager to use a single instance per request
+            app.CreatePerOwinContext(ApplicationDbContext.Create);
+            app.CreatePerOwinContext<ApplicationUserManager>(ApplicationUserManager.Create);
+
+            // Enable the application to use a cookie to store information for the signed in user
+            // and to use a cookie to temporarily store information about a user logging in with a third party login provider
+            app.UseCookieAuthentication(new CookieAuthenticationOptions());
+            app.UseExternalSignInCookie(DefaultAuthenticationTypes.ExternalCookie);
+
+            // Configure the application for OAuth based flow
+            PublicClientId = "self";
+            OAuthOptions = new OAuthAuthorizationServerOptions
+            {
+                TokenEndpointPath = new PathString("/Token"),
+                Provider = new ApplicationOAuthProvider(PublicClientId),
+                AuthorizeEndpointPath = new PathString("/api/Account/ExternalLogin"),
+                AccessTokenExpireTimeSpan = TimeSpan.FromDays(14),
+                // In production mode set AllowInsecureHttp = false
+                AllowInsecureHttp = true
+            };
+
+            // Enable the application to use bearer tokens to authenticate users
+            app.UseOAuthBearerTokens(OAuthOptions);
+
+            // Uncomment the following lines to enable logging in with third party login providers
+            //app.UseMicrosoftAccountAuthentication(
+            //    clientId: "",
+            //    clientSecret: "");
+
+            //app.UseTwitterAuthentication(
+            //    consumerKey: "",
+            //    consumerSecret: "");
+
+            //app.UseFacebookAuthentication(
+            //    appId: "",
+            //    appSecret: "");
+            
+
+            //Add Your Github client Id and secret at here.
+            app.UseGitHubAuthentication(
+              clientId: "526718130023d337b4cd",
+              clientSecret: "6f6056f77b96a61c88201e92335d355f1b2cbc3c"
+            );
+
+            //Add Your Facebook client Id and secret at here.
+            var facebookOptions = new FacebookAuthenticationOptions()
+            {
+                AppId = "669098610207056",
+                AppSecret = "4453902d7bab2cbc56e282ccf0a22b59",
+                BackchannelHttpHandler = new FacebookBackChannelHandler(),
+                UserInformationEndpoint = "https://graph.facebook.com/v2.4/me?fields=id,email"
+            };
+
+            facebookOptions.Scope.Add("email");
+            app.UseFacebookAuthentication(facebookOptions);
+
+            
+            //Add Your Gmail client Id and secret at here.
+            app.UseGoogleAuthentication(new GoogleOAuth2AuthenticationOptions()
+            {
+                ClientId = "439424713301-ln5ne0isrfrtc3cdae43v2jm053r9vsl.apps.googleusercontent.com",
+                ClientSecret = "-EDLSmmTG9T0bQ4D2bv8MN-R"
+            });
+
+          
+        }
+    }
+}
---
+++ OAuth/Scripts/oauth/social.js
@@ -0,0 +1,55 @@
+var app = angular.module("socialLogin", []);
+app.controller("loginController", function($scope) {
+
+    //Function for facebook Login.
+    $scope.facebookLogin = function(){
+        window.location.href = "/api/Account/ExternalLogin?provider=Facebook&response_type=token&client_id=self&redirect_uri=http%3A%2F%2Flocalhost%3A19576%2FLogin.html&state=GUWcHoliFgNN3dL05-rQMlDPw0nJxMaTetSEvB6zZWs1";
+
+    };
+
+    //Function for google Login.
+    $scope.googleLogin = function(){
+        window.location.href = "/api/Account/ExternalLogin?provider=Google&response_type=token&client_id=self&redirect_uri=http%3A%2F%2Flocalhost%3A19576%2FLogin.html&state=GUWcHoliFgNN3dL05-rQMlDPw0nJxMaTetSEvB6zZWs1";
+
+    };
+
+    //Function for github Login.
+    $scope.githubLogin = function () {
+
+        window.location.href = "/api/Account/ExternalLogin?provider=GitHub&response_type=token&client_id=self&redirect_uri=http%3A%2F%2Flocalhost%3A19576%2FLogin.html&state=GUWcHoliFgNN3dL05-rQMlDPw0nJxMaTetSEvB6zZWs1";
+
+    };
+
+    //Function to get access token from redirect url.
+    $scope.getAccessToken = function () {
+        if (location.hash) {
+            if (location.hash.split('access_token=')) {
+                var accessToken = location.hash.split('access_token=')[1].split('&')[0];
+                if (accessToken) {
+                    $scope.isUserRegistered(accessToken);   
+                }
+            }
+        }
+    };
+
+    //Function to make sure from backend. user is registerd or not.
+    $scope.isUserRegistered = function (accessToken) {
+        debugger;
+        $.ajax({
+            url: '/api/Account/UserInfo',
+            method: 'Get',
+            headers: {
+                'content-type': 'application/JSON',
+                'Authorization': 'Bearer ' + accessToken
+            },
+            success: function (response) {
+                debugger;
+                alert("Login Email is :" + response.Email);
+            }
+        });
+    };
+
+    //this method is called on load of Login.html to check that access token is exist or not in Login Page.
+
+    $scope.getAccessToken();
+});
